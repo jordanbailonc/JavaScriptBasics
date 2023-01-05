4d904a7edc54e21017244205bb0b2cfb2802664d
Jordan Bailon Cercado <jordanfbc@hotmail.com>
2023-01-05 20:02:48 +0100

    Write a JavaScript program to calculate multiplication

---
+++ Exercise10/exercise10.js
@@ -0,0 +1,39 @@
+
+/*Write a JavaScript program to calculate multiplication 
+and division of two numbers (input from user). */
+
+let number1;
+let number2;
+let info = document.getElementById("answ");
+
+
+function multiply(){
+
+    number1 = document.getElementById("num1").value;
+    number2 = document.getElementById("num2").value;
+
+    if(number2===undefined || number2===null|| number1==undefined|| number2===null){
+        alert('To multiply the numbers must be defined.')
+    }else{
+        let msg;
+        let result = number1*number2;
+        msg = `The result is: ${Math.round(result)}`;
+        info.textContent = msg;
+    }
+}
+
+function divide(){
+
+    number1 = document.getElementById("num1").value;
+    number2 = document.getElementById("num2").value;
+
+    if(number2===0 || number2===undefined || number2===null){
+        alert('The second number must be defined.')
+    }else{
+        let msg;
+        let result = number1/number2;
+        msg = `The result is: ${Math.round(result,2)}`;
+        info.textContent = msg
+    }
+    
+}
